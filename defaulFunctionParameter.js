c3a8e4605f62108bc5d5000f429bcfbf9e79358b
Mahadi H Ebrahim <mahadihebrahim@hotmail.com>
2021-02-01 13:19:32 +0600

    template js

---
+++ defaulFunctionParameter.js
@@ -0,0 +1,7 @@
+function sum(num1, num2 = 12) {
+    //num2 = num2 || 20; old system
+    return num1 + num2;
+}
+
+result = sum(12);
+console.log(result);
---
+++ template.js
@@ -0,0 +1,13 @@
+const firstName = 'Mahadi'
+const lastName = 'Hasan Ebrahim'
+const fullName = firstName + ' ' + lastName + " He is a good boy";
+const fullNameES = `${firstName} ${lastName} is a good boy`
+console.log(fullNameES);
+
+const multiline = "I love you\n" +
+    "I Need You\n" +
+    "I Want you";
+const multilineES = `I love you
+I need you
+I don't need you`
+console.log(multilineES);
