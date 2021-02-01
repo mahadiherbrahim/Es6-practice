3e5eef169f545caa979da175ad800f4fc0957983
Mahadi H Ebrahim <mahadihebrahim@hotmail.com>
2021-02-01 12:29:41 +0600

    Let Const Clear Concept

---
+++ letconst.js
@@ -0,0 +1,22 @@
+const profession = 'programmer';
+//profession = 'designer' find a error 
+console.log(profession);
+
+const numbers = [34, 64, 12];
+numbers[1] = 23;
+numbers.push(100);
+numbers.pop();
+//numbers = [323, 42, 42]; find a error
+
+const friend = { name: 'Ragib', phone: '0178371823' }
+console.log(friend);
+
+let patient = 'Manik Mia';
+patient = 'Fatema';
+console.log(patient);
+let sum = 0;
+for (let i = 0; i < 10; i++) {
+    const element = [i];
+
+}
+console.log(i);
